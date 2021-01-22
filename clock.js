4c160ef99ab197b1254e40558e43474a5e51c275
noelkspark <51874193+noelkspark@users.noreply.github.com>
2021-01-22 11:44:07 +0900

    first commit

---
+++ clock.js
@@ -0,0 +1,19 @@
+const clockContainer = document.querySelector(".js-clock"),
+  clockTime = clockContainer.querySelector("h1");
+
+function getTime() {
+  const date = new Date();
+  const hours = date.getHours(),
+    mins = date.getMinutes();
+
+  clockTime.innertext = `${hours < 10 ? `0${hours}` : hours} : ${
+    mins < 10 ? `0${mins}` : mins
+  }`;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
